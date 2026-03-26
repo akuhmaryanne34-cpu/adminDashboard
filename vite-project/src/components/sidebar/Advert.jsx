a92069f6fdc3cf0b65f3118544a577942d4b9aa7
Akuh Maryanne <akuhmaryanne34@gmail.com>
2026-03-26 16:55:41 +0100

    fix sidebar import in Advert

--- vite-project/src/components/sidebar/Advert.jsx
+++ vite-project/src/components/sidebar/Advert.jsx
@@ -2,7 +2,6 @@ import styles from "./Sidebar.module.css";
 
 const Advert = () => {
   return (
-    
     <div className={styles.advert}>
       <h3>Upgrade to Pro</h3>
       <p>Unlock premium tools and grow faster.</p>
